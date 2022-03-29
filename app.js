1711512d88d541c300fe2830aae83ca0a0f3361b
amywu0401 <amywu0401@yahoo.com.tw>
2022-03-29 10:31:23 +0800

    add search bar function

--- app.js
+++ app.js
@@ -30,3 +30,12 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+//設定搜尋列 (name, category)
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants, keyword })
+})
+
